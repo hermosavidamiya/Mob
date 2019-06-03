c126a2927271c5d6330351c8679fcaed3519aee6
hermosavidamiya <51322747+hermosavidamiya@users.noreply.github.com>
2019-06-03 14:27:27 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,33 @@
+{
+  "ClientId": 584733886433722388,
+  "Token": "NTg0NzMzODg2NDMzNzIyMzg4.XPPRzw.8ZSrxjALYkFscbfgC4sjn8X7t2w",
+  "OwnerIds": [
+    330398796540411904
+  ],
+  "LoLApiKey": "",
+  "GoogleApiKey": "",
+  "MashapeKey": "",
+  "OsuApiKey": "",
+  "SoundCloudClientId": "",
+  "CleverbotApiKey": "",
+  "CarbonKey": "",
+  "Db": {
+    "Type": "sqlite",
+    "ConnectionString": "Data Source=data/NadekoBot.db"
+  },
+  "TotalShards": 1,
+  "PatreonAccessToken": "",
+  "PatreonCampaignId": "334038",
+  "RestartCommand": null,
+  "ShardRunCommand": "",
+  "ShardRunArguments": "",
+  "ShardRunPort": null,
+  "MiningProxyUrl": null,
+  "MiningProxyCreds": null,
+  "BotListToken": null,
+  "TwitchClientId": null,
+  "VotesToken": null,
+  "VotesUrl": null,
+  "RedisOptions": null
+} 
+client.login(process.envy.BOT_TOKEN);
